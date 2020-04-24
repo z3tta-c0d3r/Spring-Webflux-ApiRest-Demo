3372f610f278eec62040c7a4ae6a94693b68e2a7
Zeta <vicente.garcia@outlook.com>
2020-04-24 23:11:37 +0200

    new issues

--- src/main/java/com/example/demo/SpringWebfluxApiRestDemoApplication.java
+++ src/main/java/com/example/demo/SpringWebfluxApiRestDemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import reactor.core.publisher.Flux;
 
@@ -15,6 +16,7 @@ import java.util.Date;
 
 @Slf4j
 @SpringBootApplication
+@EnableEurekaClient
 public class SpringWebfluxApiRestDemoApplication implements CommandLineRunner {
 
 	@Autowired
